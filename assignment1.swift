0261699945f5cef8943dc90f5c169a65af8d87d9
rizetap <rizetapq@gmail.com>
2025-09-22 13:48:27 +0500

    assignmeny1

---
+++ assignment1.swift
@@ -0,0 +1,31 @@
+var firstName: String = "Perizat"
+var lastName: String = "Muratova"
+//var age: Int = 20
+var birthYear: Int = 2005
+var isStudent: Bool = true
+var height: Double = 1.65
+var major: String = "Information Technology"
+var hometown: String = "Aktobe"
+
+var hobbies = ["Painting", "Watching movies", "Listening to music", "Cooking"]
+
+let currentYear = 2025
+
+var age = currentYear - birthYear
+
+print(age)
+
+var hobby: String = "Drawing"
+var numberOfHobbies: Int = 3
+var favoriteNumber: Int = 21
+var isHobbyCreative: Bool = true
+var favoriteBand: String = "Svidaniye"
+var favoriteMovie: String = "The Fly"
+var siblings = ["Zhazira", "Aizere", "Ibrahim"]
+var numberOfSiblings: Int = siblings.count
+
+print(numberOfSiblings)
+
+var futureGoals: String = "study abroad"
+
+print("Hello! My name is \(firstName) \(lastName). Im \(age) years old and im majoring \(major). Im from \(hometown). I have \(numberOfSiblings) and their names are \(siblings) siblings. In my free time i like \(hobbies). My favorite music band is \(favoriteBand) and my favorite movie is '\(favoriteMovie)'. My lucky number is \(favoriteNumber). I hope in the future i will \(futureGoals).") 
